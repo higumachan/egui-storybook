89a9e6eaa959fcf1ff39425ab99da4b3e745f329
Yuta Hinokuma <higumachan@users.noreply.github.com>
2024-03-26 00:14:13 +0900

    add image example

--- demo/demo_project/examples/storybook.rs
+++ demo/demo_project/examples/storybook.rs
@@ -1,4 +1,5 @@
 use eframe::NativeOptions;
+use egui::{Image, Pos2, Rect, Vec2};
 use egui_storybook::story_book::{Story, StoryBookBuilder};
 use egui_storybook::{run_story_book, story_body};
 
@@ -20,6 +21,19 @@ fn main() {
                 });
             },
         ))
+        .add_story(
+            Story::new(
+                "image",
+                story_body! {
+                    use egui::*;
+
+                    egui::CentralPanel::default().show(ctx, |ui| {
+                        ui.add(Image::from_bytes("bytes://logo.png", include_bytes!("../assets/logo.png")));
+                    });
+                },
+            )
+            .add_asset_file("./assets/logo.png".into()),
+        )
         .build();
 
     run_story_book("demo_project", story_book, NativeOptions::default()).unwrap();
